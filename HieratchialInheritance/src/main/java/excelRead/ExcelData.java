eb6480821a14841badbddb4dc5f34f5fd19f50d3
amrit <amrit@192.168.1.39>
2024-07-10 11:42:52 +0530

    New sheet added to excel.

--- HieratchialInheritance/src/main/java/excelRead/ExcelData.java
+++ HieratchialInheritance/src/main/java/excelRead/ExcelData.java
@@ -1,5 +1,6 @@
 package excelRead;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -9,7 +10,8 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 	{
 		public static FileInputStream f;
 		public static XSSFWorkbook w;
-		public static XSSFSheet sh;
+		public static XSSFSheet sh,sh1;
+		
 	
     public static String getStringData(int a,int b) throws IOException
     {
@@ -18,13 +20,32 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
     	sh=w.getSheet("IDName");
     	Row r=sh.getRow(a);//Interface
     	Cell c=r.getCell(b);//Interface
-    	return c.getStringCellValue();//method for returning cell value
+    	return c.getStringCellValue();//method for returning cell value    
     }
     public static String getIntegerData(int a,int b) throws IOException
     {
     	f=new FileInputStream("C:\\Users\\amrit\\git\\Inheritance\\HieratchialInheritance\\src\\main\\resources\\TestData.xlsx");
     	w=new XSSFWorkbook(f);
     	sh=w.getSheet("IDName");
+    	Row r=sh.getRow(a);//Interface
+    	Cell c=r.getCell(b);//Interface
+    	int x=(int) c.getNumericCellValue();//type casting String value to integer value
+    	return String.valueOf(x);
+    }
+    public static String getStringData1(int a,int b) throws IOException 
+    {
+    	f=new FileInputStream("C:\\Users\\amrit\\git\\Inheritance\\HieratchialInheritance\\src\\main\\resources\\TestData.xlsx");
+    	w=new XSSFWorkbook(f);
+       	sh1=w.getSheet("Marks");
+    	Row r=sh.getRow(a);
+    	Cell c=r.getCell(b);
+    	return c.getStringCellValue();
+    }    
+    public static String getIntegerData1(int a,int b) throws IOException
+    {
+    	f=new FileInputStream("C:\\Users\\amrit\\git\\Inheritance\\HieratchialInheritance\\src\\main\\resources\\TestData.xlsx");
+    	w=new XSSFWorkbook(f);
+    	sh=w.getSheet("Marks");
     	Row r=sh.getRow(a);//Interface
     	Cell c=r.getCell(b);//Interface
     	int x=(int) c.getNumericCellValue();//type casting String value to integer value
--- HieratchialInheritance/src/main/java/excelRead/ExcelMain.java
+++ HieratchialInheritance/src/main/java/excelRead/ExcelMain.java
@@ -9,6 +9,12 @@ public class ExcelMain
 		System.out.println(ExcelData.getStringData(2,0));
 		System.out.println(ExcelData.getIntegerData(2,1));
 		System.out.println(ExcelData.getStringData(3,0));
-		System.out.println(ExcelData.getIntegerData(3,1));
+		System.out.println(ExcelData.getIntegerData(3,1));		
+		System.out.println(ExcelData.getStringData1(1,0));
+		System.out.println(ExcelData.getIntegerData1(1,1));	
+		System.out.println(ExcelData.getStringData1(2,0));
+		System.out.println(ExcelData.getIntegerData1(2,1));
+		System.out.println(ExcelData.getStringData1(3,0));
+		System.out.println(ExcelData.getIntegerData1(3,1));
 	}
 }
