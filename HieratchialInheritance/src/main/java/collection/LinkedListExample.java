9f6398a3581a54eb99d1dd5f0320c015e796e5fd
amrit <amrit@192.168.1.39>
2024-07-11 17:15:53 +0530

    Updated Linked List program with remove() for Integer value.

--- HieratchialInheritance/src/main/java/collection/LinkedListExample.java
+++ HieratchialInheritance/src/main/java/collection/LinkedListExample.java
@@ -6,20 +6,34 @@ public class LinkedListExample
 	public static void main(String[] args)
 	{
 		LinkedList<String> obj=new LinkedList<String>();
+		LinkedList<Float> obj2=new LinkedList<Float>();
 		obj.add("Amritha");
 		obj.add("Lakshmi");
 		obj.add("Vyshnavi");
 		obj.add("Akhil");
 		obj.add("Vishnu");
 		obj.add("Abhiram");
+		obj2.add(11.5f);
+		obj2.add(20.6f);
 		int size=obj.size();
 		System.out.println("Size:"+size);
-		System.out.println(obj);	
+		System.out.println(obj);
+		int size1=obj2.size();
+		System.out.println("Size:"+size1);
+		System.out.println(obj2);
 		Iterator<String> obj1=obj.iterator();
 		while(obj1.hasNext())
 		{
 			System.out.println(obj1.next());
 			//System.out.println(obj1.remove());
+		}
+		Iterator obj3=obj2.iterator();
+		while(obj3.hasNext())
+		{
+			System.out.println(obj3.next());
+			System.out.println("Removed Elements");
+			System.out.println(obj2.remove(1));	
+			System.out.println(obj2.remove(0));	
 		}
 	}
 }
